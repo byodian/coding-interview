c49cc5779e4e51b91b63a86b51a938f4b79fcbbb
byodian <byj6696@gmail.com>
2022-05-29 00:50:08 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
